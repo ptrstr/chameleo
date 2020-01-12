6cc6eda22402370290a60ee9700e28faf82446be
ptrstr <ptrstr@protonmail.com>
2020-01-12 17:01:28 -0500

    Updated build.sh to work on any platform

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
@@ -10,4 +10,6 @@ cmake ..
 make
 
 # Binary will be stegano, but be sure to make this cross-project
-su -c "mv ./stegano /usr/bin/stegano && sign /usr/bin/stegano"
+if [ "$(uname -sp)" == "Darwin arm64" ]; then
+  su -c "mv ./stegano /usr/bin/stegano && sign /usr/bin/stegano"
+fi
